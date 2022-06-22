4cb3a4be0b00864a951c0d00648c4b44389369e8
aprillasatya <aprila.satya@gmail.com>
2022-06-22 09:35:01 +0700

    updated all

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ window.addEventListener("scroll", function () {
     nav.classList.remove("shadow");
   }
 });
+
+/*************
+ Contact Form
+ *************/
+const scriptURL = "https://script.google.com/macros/s/AKfycbxL1AO1fvSIx4VRsItakTyqsWkXSlIHyWpPgXqn3ASoxULZhPaiRTYworM-uFGDyYJF/exec";
+const form = document.forms["contact-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // when submit clicked
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // show alert
+      myAlert.classList.toggle("d-none");
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
